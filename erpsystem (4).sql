02ed2aba6da25c27dd9bee4f34a67ce5e3355554
Patryk Kozłowski <76659960+IceStormFox@users.noreply.github.com>
2023-07-13 18:54:27 +0200

    Update erpsystem.sql
    Cleanup

--- erpsystem (4).sql
+++ erpsystem (4).sql
@@ -44,12 +44,7 @@ CREATE TABLE `rejestracja` (
 INSERT INTO `rejestracja` (`id`, `nazwa`, `haslo`, `email`, `rola`, `token`, `aktywacja`) VALUES
 (1, 'test', '05a671c66aefea124cc08b76ea6d30bb', 'test@wp.pl', 0, NULL, 0),
 (2, 'admin', 'b73ca81b9a698682644461cd0e92a81c', 'carton@carton.pl', 2, NULL, 1),
-(3, 'przelozony', '1751281ec12448c2d4709cf36832ea51', 'przelozony@test.pl', 1, NULL, 1),
-(9, 'test1234', '16d7a4fca7442dda3ad93c9a726597e4', 'test1234@wp.pl', 0, NULL, 1),
-(13, 'test2', '05a671c66aefea124cc08b76ea6d30bb', 'test@test.test', 0, NULL, 0),
-(16, 'token123', '1fc34072660678ab6395a990ca908258', 'token123@token.pl', 0, '391b8272ad3298027c0789338cb5a046', 0),
-(18, '12341234', 'ed2b1f468c5f915f3f1cf75d7068baae', '12341234@12.pl', 0, 'c1505d22561947827d66643334236310', 0),
-(39, 'Patryk', '707d4571464411e49e44befdd1892582', 'patryk_kozlowski@yahoo.com', 0, 'd5c38dc50f0bee13c712c470b36b0e9d', 1);
+(3, 'przelozony', '1751281ec12448c2d4709cf36832ea51', 'przelozony@test.pl', 1, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -71,10 +66,9 @@ CREATE TABLE `zarejestrowanegodziny` (
 --
 
 INSERT INTO `zarejestrowanegodziny` (`id`, `nazwa`, `godziny`, `data_pracy`, `weryfikacja`, `komentarz`) VALUES
-(22, 'test1234', 4, '2023-06-14', 'Zaakceptowane', 'xd'),
-(23, 'test1234', 8, '2023-06-16', 'Do zatwierdzenia', 'halo kartus halo'),
-(24, 'test1234', 2, '2023-06-01', 'Do zatwierdzenia', 'topory'),
-(25, 'Patryk', 8, '2023-06-08', 'Zaakceptowane', 'Hello check test');
+(1, 'test', 4, '2023-06-14', 'Zaakceptowane', 'Accepted check'),
+(2, 'test', 8, '2023-06-16', 'Do zatwierdzenia', 'To accept check'),
+(3, 'test', 2, '2023-06-01', 'Do zatwierdzenia', 'Test');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -100,13 +94,13 @@ ALTER TABLE `zarejestrowanegodziny`
 -- AUTO_INCREMENT dla tabeli `rejestracja`
 --
 ALTER TABLE `rejestracja`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT dla tabeli `zarejestrowanegodziny`
 --
 ALTER TABLE `zarejestrowanegodziny`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
